c0e418324ed7126dc39b8f48053c218a6a215bb4
hjj_glb <hu468502553_glb@qq.com>
2022-03-14 21:41:39 +0800

    搭建项目结构

---
+++ austin-web/src/main/java/com/bigotry/austin/AustinApplication.java
@@ -0,0 +1,19 @@
+package com.bigotry.austin;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
+
+/**
+ * @author £ºJJHu
+ * @date £ºCreated in 2022/3/14 21:04
+ * @description£º
+ * @modified By£º
+ * @version: $
+ */
+@SpringBootApplication
+@RestController
+public class AustinApplication {
+    private final Logger
+}
